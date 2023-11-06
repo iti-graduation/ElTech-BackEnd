521511500001559dff0e1ccbb539a772d5f6ca87
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-11-06 13:47:02 +0200

    PX-61 Create main project models

--- eltech/core/models.py
+++ eltech/core/models.py
@@ -1,3 +1,213 @@
-from django.db import models
+""" Database models """
 
-# Create your models here.
+import uuid
+import os
+
+from django.conf import settings
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+
+def image_file_path(instance, filename, image_parent):
+    """Generate file path for new image"""
+
+    # get the extension of the file (png/jpg/etc..)
+    ext = os.path.splitext(filename)[1]
+    filename = f'{uuid.uuid4()}{ext}'
+
+    return os.path.join('uploads', image_parent, filename)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user"""
+
+        if not email:
+            raise ValueError('User must have an email address.')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        """Create, save and return a new superuser"""
+
+        user = self.create_user(email=email, password=password, **extra_fields)
+
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+
+
+class Category(models.Model):
+    """Category object"""
+    name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to=lambda instance, filename: image_file_path(instance, filename, 'category'))
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """Product object"""
+    name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    stock = models.PositiveIntegerField()
+    is_hot = models.BooleanField(default=False)
+    is_on_sale = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    """Rating object"""
+    rating = models.PositiveIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)],
+        null=True,
+        default=1
+    )
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Review(models.Model):
+    """Review object"""
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class ProductImage(models.Model):
+    """Product image object"""
+    image = models.ImageField(upload_to=lambda instance, filename: image_file_path(instance, filename, 'product'))
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
+
+
+class ProductFeature(models.Model):
+    """Product feature object"""
+    feature = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='features')
+
+    def __str__(self):
+        return self.feature
+
+
+class Favorite(models.Model):
+    """Favorite product object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Coupon(models.Model):
+    """Coupon object"""
+    code = models.CharField(max_length=255)
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Cart(models.Model):
+    """Cart object"""
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='CartProduct')
+    coupon = models.ForeignKey(Coupon, on_delete=models.SET_NULL, null=True, blank=True)
+
+    @property
+    def total_price(self):
+        total = sum(cp.total_price for cp in self.cartproduct_set.all())
+        if self.coupon:
+            total *= (1 - self.coupon.discount)
+        return total
+
+
+class CartProduct(models.Model):
+    """Cart product object"""
+    quantity = models.PositiveIntegerField(default=1)
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    @property
+    def total_price(self):
+        return self.product.price * self.quantity
+
+
+class Order(models.Model):
+    """Order object"""
+    STATUS_CHOICES = (
+        ('pending', 'Pending'),
+        ('shipped', 'Shipped'),
+        ('delivered', 'Delivered'),
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    total_price = models.DecimalField(max_digits=5, decimal_places=2)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderProduct')
+
+
+class OrderProduct(models.Model):
+    """Order product object"""
+    quantity = models.PositiveIntegerField(default=1)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    """Post object"""
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    image = models.ImageField(upload_to=lambda instance, filename: image_file_path(instance, filename, 'post'))
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    """Comment object"""
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='replies')
