ae3921fab0b58f94d70e994368244aeb2c6b96d4
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-11-06 15:47:03 +0200

    PX-61 Register admin models

--- eltech/core/admin.py
+++ eltech/core/admin.py
@@ -1,3 +1,69 @@
-from django.contrib import admin
+""" Django Admin Customization """
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as tr
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users."""
+
+    ordering = ['id']
+    list_display = ['email', 'name']
+    readonly_fields = ['last_login']
+    fieldsets = (
+        (
+            None,
+            {
+                'fields': (
+                    'email',
+                    'password'
+                )
+            }
+        ),
+        (
+            tr('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+        (
+            tr('Important dates'),
+            {
+                'fields': (
+                    'last_login',
+                )
+            }
+        )
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                'classes': ('wide',),
+                'fields': (
+                    'email',
+                    'password1',
+                    'password2',
+                    'name',
+                    'is_active',
+                    'is_staff',
+                    'is_superuser'
+                )
+            }
+        ),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
+admin.site.register(models.Product)
+admin.site.register(models.Category)
+admin.site.register(models.Post)
+admin.site.register(models.Order)
+admin.site.register(models.Cart)
