6f69922a05e20a1f9e5b2e6912ac06cdcdbceb7c
Ammar M. Kamel <ammarkamel9899@gmail.com>
2023-11-09 20:48:36 +0200

    PX-119 Implement retrieving featured products only

--- eltech/product/views.py
+++ eltech/product/views.py
@@ -20,11 +20,35 @@ from core.models import Product
 from product import serializers
 
 
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                "is_featured",
+                OpenApiTypes.INT,
+                enum=[0, 1],
+                description="Get featured products only.",
+            )
+        ]
+    )
+)
 class ProductViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, viewsets.GenericViewSet):
     """Views for manage product APIs."""
 
     serializer_class = serializers.ProductDetailSerializer
     queryset = Product.objects.all()
+
+    def get_queryset(self):
+        """Filter queryset for products."""
+        is_featured = bool(int(self.request.query_params.get("is_featured", 0)))
+        queryset = self.queryset
+
+        if is_featured:
+            queryset = queryset.filter(is_featured=True)
+        else:
+            queryset = queryset.filter(is_featured=False)
+
+        return queryset.order_by("-id")
 
     def get_serializer_class(self):
         """Return the serializer class for request."""
