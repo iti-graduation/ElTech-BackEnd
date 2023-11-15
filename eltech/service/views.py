ebaa5a3271496e94a752f2504a2ddf3915d4a246
Abdulrahman Fiala <abdulrahmanFiala@gmail.com>
2023-11-15 16:04:46 +0200

    Editing the Service app to be editable only to admins

--- eltech/service/views.py
+++ eltech/service/views.py
@@ -1,38 +1,28 @@
 from rest_framework import viewsets, status
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.response import Response
 
 from core.models import Service
-from service import serializers  
+from service import serializers
 
 
-class ServiceViewSet(viewsets.ModelViewSet):
+class ServiceViewSet(viewsets.ReadOnlyModelViewSet):
     """View for the manage service APIs"""
-    serializer_class = serializers.ServiceDetailSerializer
+    serializer_class = serializers.ServiceSerializer
     queryset = Service.objects.all()
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get_permissions(self):
-        if self.action in ['retrieve', 'list']:
+        if self.action in ['list']:
             return [AllowAny()]
+        elif self.action in ['retrieve', 'create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
         return super().get_permissions()
 
     def get_serializer_class(self):
         """Return the serializer class for request"""
         if self.action == 'list':
             return serializers.ServiceSerializer
-
         return self.serializer_class
-
-    def perform_create(self, serializer):
-        """Create a new service"""
-        title = serializer.validated_data['title']
-        existing_service = Service.objects.filter(title=title)
-
-        if existing_service.exists():
-            raise serializers.ValidationError(
-                "Service with this title already exists.")
-
-        serializer.save()
